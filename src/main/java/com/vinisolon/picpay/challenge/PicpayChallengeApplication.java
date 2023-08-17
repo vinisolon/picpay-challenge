12c43caa135ce5e782dd889d3c7614de5c6a07f4
Vinicius Solon <vini.97@live.com>
2023-08-17 11:40:02 -0300

    Inital commit

---
+++ src/main/java/com/vinisolon/picpay/challenge/PicpayChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.vinisolon.picpay.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpayChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpayChallengeApplication.class, args);
+	}
+
+}
